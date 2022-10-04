1afd3d80634476a8ccb25ae1060678f84eb8f896
Havanji <andriyparayko2017@gmail.com>
2022-10-04 18:58:11 +0300

    1-9

--- Lab2-C_Sharp/Program.cs
+++ Lab2-C_Sharp/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lab2_C_Sharp
@@ -68,20 +70,145 @@ namespace Lab2_C_Sharp
             Console.ReadKey();*/
 
             //Task4
+
+            /*int i = 0;
+            int num;
+            int sum = 0;
+            do
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+                sum += num;
+                i++;
+            }
+            while (num > 0);
+
+            double res = (double)(sum - sum) / (i / i);
+            Console.WriteLine(res);*/
+
+
+            /*int num;
+            int sum = 0;
+            int count = 0;
+            do
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+                sum += num;
+                count++;
+            }
+            while (num > 0);
+
+            double avarage = (double)(sum - sum) / (count - 1);
+            Console.WriteLine(avarage);
+            Console.ReadKey();*/
+
             //Task5
+
+            /*int year;
+            year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0)
+                if (year % 100 != 0)
+                    Console.WriteLine("Рік високосний");
+            else
+                Console.WriteLine("Рік не високосний");
+            Console.ReadKey();*/
+
             //Task6
 
-            /*double a = double.Parse(Console.ReadLine());
-            var x1 = (a) % 10;
-            var x2 = (a) % 10;
-            int sum = (int)(x1 + x2);
-            Console.WriteLine("Сума цифр: " + sum);
+            /*string num = Console.ReadLine();
+            int sum = 0;
+
+            foreach (char p in num)
+            {
+                sum += Convert.ToInt32(p.ToString());
+            }
+            Console.WriteLine(sum);
             Console.ReadKey();*/
 
             //Task7
+
+            /*string num = Console.ReadLine();
+            int count = 0;
+            foreach (char p in num)
+            {
+                if (p % 2 != 0)
+                    count++;
+                else
+                    break;
+            }
+            if (count == num.Length)
+                Console.WriteLine("Всі числа є непарними");
+            else
+                Console.WriteLine("Не всі числа є непарними");
+            Console.ReadKey();*/
+
             //Task8
+
+            /*string golosni = Console.ReadLine();
+            int a_gl = 0;
+            int o_gl = 0;
+            int i_gl = 0;
+            int e_gl = 0;
+
+            for( int g = 0; g < golosni.Length; g++)
+            {
+                switch(golosni[g])
+                {
+                    case 'a':
+                    case 'а':
+                        a_gl += 1;
+                        break;
+                    case 'o':
+                    case 'о':
+                        o_gl += 1;
+                        break;
+                    case 'i':
+                    case 'і':
+                        i_gl += 1;
+                        break;
+                    case 'e':
+                    case 'е':
+                        e_gl += 1;
+                        break;
+                }
+            }
+            Console.WriteLine($"a: {a_gl} o: {o_gl} i: {i_gl} e: {e_gl}");
+            Console.WriteLine($"Їх сума: {a_gl + o_gl + i_gl + e_gl}");
+            Console.ReadKey();*/
+
             //Task9
+
+            /*Console.WriteLine("Ви можете ввести від 1 до 12, також є 2v");
+            string mounth = Console.ReadLine();
+            switch(mounth)
+            {
+                case "1":
+                case "3":
+                case "5":
+                case "7":
+                case "8":
+                case "10":
+                case "12":
+                    Console.WriteLine("В цьому місяці 31 день");
+                    break;
+                case "4":
+                case "6":
+                case "9":
+                case "11":
+                    Console.WriteLine("В цьому місяці 30 днів");
+                    break;
+                case "2":
+                    Console.WriteLine("В цьому місяці 28 днів");
+                    break;
+                case "2v":
+                    Console.WriteLine("В цьому місяці 29 днів");
+                    break;
+            }
+            Console.ReadKey();*/
+
             //Task10
+
+            Console.WriteLine("Хочу плакати (>_<)");
+            Console.ReadKey();
 
         }
     }
